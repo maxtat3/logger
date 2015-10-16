fce916c5ff8a3d122dc1ed0711ed3fb9490d7f81
Max <multi3volume@gmail.com>
2015-10-16 23:09:44 +0300

    МК: целевая прощивка v0.3 В этой версии дополнительно реализована логика обработки выбора количества выборок(семплов) в секунду (задает пользователь): 15 выборок/секунду (максимум) = 0 мс ;  10 выборок/секунду = 25 мс ; 4 выборок/секунду = 57 мс . Также по сравнению с спредыдущей версией (v0.2) уменьшена скорость передачи данных с 19200 до 9600 бод. На количество  выборок в секунду это почти не повлияло. Но это позволит применить схему согласования уровней на tiny2313 (USB-USART). Она поддерживает максимальную скорость в 9600 бод. Реализованы две тестовые ф-ии на светодиодах blik_led1() и blik_led2(). Упорядочены комментарии в коде.

--- firmware/main.c
+++ firmware/main.c
@@ -2,21 +2,17 @@
 //
 //	Description.: осциллограф-самописец
 //
-//	Version.....: 0.2
+//	Version.....: 0.3
 //
 //  Target(s) mcu...: mega8
 //
 //  Compiler....: gcc-4.3.3 (WinAVR 2010.01.10)
 //
-//	Comment(s)..: только при приеме по USART символов '1', '2', '3', '4'  мк посылает
-//				значение 1-го  2-го  3-го 4-го канала ацп преобразования соответственно. 
-//				<!-- Т.к. результат каждого ацп преобразования
-//				представляет собой 10-и битный результат, а данные передаются по 8-и 
-//				битному каналу, то для для того чтобы передать значение измеренноое ацп
-//				нужно результат преобразования делить на 4. В итоге в программе на ПК
-//				будкт строится графики по 8-и битным значениям. -->
+//	Comment(s)..: добавлена возможность установки количества
+//					выборок (семплов) в секунду.
+//					добавлены две тестовые ф-ии на светодиодах.
 //
-//  Data........: 09.05.14
+//  Data........: 10.05.14
 //	
 //***************************************************************************
 
@@ -25,10 +21,12 @@
 #include <util/delay.h>
 #include <util/atomic.h>
 
+#define nop()  __asm__ __volatile__("nop")
+
 #define		LED1	0b00001000
-// #define LED2 0b00000100
+#define		LED2	0b00010000
 #define		OFF_ALL_LEDS	0x00
-#define		LED_PAUSE		_delay_ms(500)
+#define		LED_PAUSE		_delay_ms(100)
 
 #define		HIGH_7	(_BV(7))
 #define		HIGH_6	(_BV(6))
@@ -46,38 +44,39 @@
 #define		LOW_2	(~(_BV(2)))
 #define		LOW_1	(~(_BV(1)))
 
-// #define 	ADC1_REFINT		(_BV(7))|(_BV(6))|(~(_BV(5)))|(~(_BV(4)))|(~(_BV(3)))|(~(_BV(2)))|(~(_BV(1)))|(_BV(0))
-// #define 	ADC1_REFINT		HIGH_7 | HIGH_6 | LOW_5 | LOW_4 | LOW_3 | LOW_2 | LOW_1 | HIGH_0
 #define 	ADC1_REFINT		HIGH_7 | HIGH_6 | HIGH_0
 #define 	ADC2_REFINT		HIGH_7 | HIGH_6 | HIGH_1
 #define 	ADC3_REFINT		HIGH_7 | HIGH_6 | HIGH_1 | HIGH_0
 #define 	ADC4_REFINT		HIGH_7 | HIGH_6 | HIGH_2	
-
 // #define 	ADC1_REFINT		0b11000001	//внутр ИОН=2,56В; active ADC1
 // #define 	ADC2_REFINT		0b11000010	//внутр ИОН=2,56В; active ADC2
 // #define 	ADC3_REFINT		0b11000011	//внутр ИОН=2,56В; active ADC3
 
-
-
+#define		DELAY_15_SPS_4CH		nop()
+#define		DELAY_10_SPS_4CH		_delay_ms(25)
+#define		DELAY_4_SPS_4CH			_delay_ms(57)
 
 
 void init_io(void);
 void init_adc(void);
 void init_usart(void);
 void blik_led1(void);
-// void blik_led2(void);
+void blik_led2(void);
 void sendCharToUSART(unsigned char sym);
 unsigned char getCharOfUSART(void);
 
-//однобайтный буфер
-volatile unsigned char usartRxBuf = 0;
+
+volatile unsigned char usartRxBuf = 0;	//однобайтный буфер
 volatile unsigned int val1, val2, val3, val4;
 volatile unsigned char lowByte;
 volatile unsigned int adcResult;
+volatile unsigned char requestToChangeSamplePerSecond = 0;
+volatile unsigned char userSamplesPerSecond = 0;
 
 
 int main(void){
 	unsigned char sym;
+	
 	cli();
 	init_io();
 	init_adc();
@@ -88,6 +87,17 @@ int main(void){
 	
 	while(1){
 		sym = getCharOfUSART();
+
+		if(requestToChangeSamplePerSecond == 1){
+			if(sym == '0' || sym == '1' || sym == '2'){
+				userSamplesPerSecond = sym;
+				requestToChangeSamplePerSecond = 0;
+			}
+		}
+		if(sym == 's'){
+			requestToChangeSamplePerSecond = 1;
+		}
+		
 		if(sym == '1'){
 			sendCharToUSART((unsigned char)(val1/4));
 		}else if(sym == '2'){
@@ -97,18 +107,27 @@ int main(void){
 		}else if(sym == '4'){
 			sendCharToUSART((unsigned char)(val4/4));
 		}
+		if(userSamplesPerSecond == '0'){
+			DELAY_15_SPS_4CH;
+		}else if(userSamplesPerSecond == '1'){
+			DELAY_10_SPS_4CH;
+		}else if(userSamplesPerSecond == '2'){
+			DELAY_4_SPS_4CH;
+		}
+		
 	}
-	
 	// while(1){
 		// blik_led1();
 	// }
 }
-//===========================
-//прием символа по usart`у в буфер
+
+
+// прием символа по usart`у в буфер
 ISR(USART_RXC_vect){ 
    usartRxBuf = UDR;  
 } 
 
+// обработка прерывания от ацп
 ISR(ADC_vect){
 	// 1. считываем младший и старший байты результата АЦ-преобразования и образуем из них 10-битовый результат
 	lowByte = ADCL;
@@ -139,12 +158,15 @@ ISR(ADC_vect){
 	// 3. запускаем новое АЦ-преобразование
 	ADCSRA |= (1<<ADSC);
 }
-//===========================
 
+
+// настройка портов в/в
 void init_io(void){
-	DDRB = (_BV(3));
+	DDRB |= (_BV(3));
+	DDRB |= (_BV(4));
 }
 
+// настройка АЦП
 void init_adc(void){
 	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); // предделитель на 128
 	ADCSRA |= (1<<ADIE);                        // разрешаем прерывание от ацп
@@ -155,53 +177,30 @@ void init_adc(void){
 	//ADMUX|=0b11000001;
 }
 
+// настройка USART
 void init_usart(void){
-	// Table 82 rus datasheet mega8
-	// UBRRL = 103; //103 -скорость обмена 1200 бод для 2 Mhz cpu
-	// UBRRL = 25; //25 -скорость обмена 4800 бод для 2 Mhz cpu
-	// UBRRL = 12; //12 -скорость обмена 9600 бод для 2 Mhz cpu
-	
-	
-	// Значение которое пишется в UBRR зависит от тактовой частоты MCU
-	// Вычисляется по формуле из табл. 52 datasheet на atmega8
-	// Например для fcpu = 8 MHz, для асинхронного нормального режима
-	// и требуемой скорости 9600 бод : UBBR = (8 MHz)/16*9600 - 1 = 51
-	
-	//UBRR=766 @ 1200 бод при 14,7456 MHz (U2X = 0)
-	// UBRRH = 0x02;
-	// UBRRL = 0xFE; 
+	// UBRR=47 @ 19200 бод при 14,7456 MHz (U2X = 0)
+	// самый оптимальный вариант (16 выб/с для 4 канала)
+	// UBRRH = 0;
+	// UBRRL = 47; 
 	
 	//UBRR=95 @ 9600 бод при 14,7456 MHz (U2X = 0)
-	// UBRRH = 0;
-	// UBRRL = 95; 
-	
-	//UBRR=47 @ 19200 бод при 14,7456 MHz (U2X = 0)
-	// --------- самый оптимальный вариант (16 выб/с для 4 канала)
+	// примерно 15 выб/с для 4 канала
 	UBRRH = 0;
-	UBRRL = 47; 
-	
-	//UBRR=23 @ 38400 бод при 14,7456 MHz (U2X = 0)
-	// UBRRH = 0;
-	// UBRRL = 23; 
-	
-	//UBRR=15 @ 57600 бод при 14,7456 MHz (U2X = 0)
-	// UBRRH = 0;
-	// UBRRL = 15; 
+	UBRRL = 95; 
 	
 	// UCSRA=(1<<U2X);
-	
 	UCSRB=(1<<RXCIE)|(1<<RXEN)|(1<<TXEN); //разр. прерыв при приеме, разр приема, разр передачи.
 	UCSRC=(1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);  //размер слова 8 разрядов
 }
-//=====================================
 
-//отправка символа по usart`у
+// отправка символа по usart`у
 void sendCharToUSART(unsigned char sym){
 	while(!(UCSRA & (1<<UDRE)));
 	UDR = sym;  
 }
 
-//чтение буфера
+// чтение буфера
 unsigned char getCharOfUSART(void){
 	unsigned char tmp;
 	ATOMIC_BLOCK(ATOMIC_FORCEON){
@@ -211,24 +210,18 @@ unsigned char getCharOfUSART(void){
 	return tmp;  
 }
 
-/*
-	Тестовая ф-ия для проверки правильности работы макросов
-*/
+// тестовая ф-ия 1
 void blik_led1(void){
-	//сюда нужно подставить проверяемый макрос
-	// if(0b11000001 == 0b11000011){
-	// if( ((_BV(7)) | (_BV(6)) | (_BV(0))) == 0b11000011){
-	if( (ADC1_REFINT) == 0b11000001){
-		PORTB = LED1;
-		LED_PAUSE;
-		PORTB = OFF_ALL_LEDS;
-		LED_PAUSE;
-	}
+	PORTB = LED1;
+	LED_PAUSE;
+	PORTB = OFF_ALL_LEDS;
+	LED_PAUSE;
 }
 
-// void blik_led2(void){
-	// PORTB = LED2;
-	// _delay_ms(LED_PAUSE);
-	// PORTB = OFF_ALL_LEDS;
-	// //_delay_ms(300);
-// }
+// тестовая ф-ия 2
+void blik_led2(void){
+	PORTB = LED2;
+	LED_PAUSE;
+	PORTB = OFF_ALL_LEDS;
+	LED_PAUSE;
+}
