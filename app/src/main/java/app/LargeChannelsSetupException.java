a5b1f4a52716356f3a18d7e4caa8debbb719b6c0
maxtat3 <multi3volume@gmail.com>
2016-03-30 20:10:44 +0300

    LargeChannelsSetupException added for previous commit.

---
+++ app/src/main/java/app/LargeChannelsSetupException.java
@@ -0,0 +1,26 @@
+package app;
+
+/**
+ * Thrown when try to set a greater number of channels that can to accept handler.
+ * Handler is set user selected channels in Controller by preparing to measure process.
+ *
+ * @see Controller#setChannelsNum(int)
+ */
+public class LargeChannelsSetupException extends Exception{
+
+    /**
+     * Constructs an <code>LargeChannelsSetupException</code> with not detail message.
+     */
+    public LargeChannelsSetupException() {
+        super();
+    }
+
+    /**
+     * Constructs an <code>LargeChannelsSetupException</code> with the specified detail message.
+     *
+     * @param message detail message.
+     */
+    public LargeChannelsSetupException(String message) {
+        super(message);
+    }
+}
