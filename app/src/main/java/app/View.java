9a27a13b0cd823b422496186e80ff3d45705eaac
maxtat3 <multi3volume@gmail.com>
2016-04-16 08:48:53 +0300

    View components direction panel creating when View object init. Initialize and settings this components performed AFTER create Controller and Model objects. Because in startup application we need to get from this objects (sps). For this moved single method initDirPanelComponents().

--- app/src/main/java/app/View.java
+++ app/src/main/java/app/View.java
@@ -47,8 +47,15 @@ public class View extends ApplicationFrame implements ViewCallback{
 	private TimeSeries series3;
 	private TimeSeries series4;
 
-	public Label lbPortState;
-    static Class class$org$jfree$data$time$Millisecond; /* synthetic field */
+	// View components
+	private final JComboBox<String> cmbCOMPortNumber = new JComboBox<String>();
+	private final JComboBox<String> cmbNumberOfChannels = new JComboBox<String>();
+	private final JComboBox<String> cmbSpsSelector = new JComboBox<String>();
+	private Label lbPortState = new Label();
+	private Checkbox chbRecord = new Checkbox();
+	private final JButton btnStartProcess = new JButton(START_STOP_TXT);
+
+	private static Class class$org$jfree$data$time$Millisecond; /* synthetic field */
     private Controller controller;
 
 
@@ -63,6 +70,7 @@ public class View extends ApplicationFrame implements ViewCallback{
         setContentPane(mainPanel);
         controller = new Controller(this);
         controller.turnOnUSART(USART.NUMBERS_OF_COM_PORTS[16]);
+		initDirPanelComponents();
     }
 
 	/**
@@ -84,72 +92,6 @@ public class View extends ApplicationFrame implements ViewCallback{
 			ChartPanel chartpanel = new ChartPanel(createChart(tsc1, tsc2, tsc3, tsc4));
 			chartpanel.setPreferredSize(new Dimension(900, 500));
 
-			// Init view components
-			final JComboBox<String> cmbCOMPortNumber = new JComboBox<String>();
-			final JComboBox<String> cmbNumberOfChannels = new JComboBox<String>();
-			final JComboBox<String> cmbSpsSelector = new JComboBox<String>();
-			lbPortState = new Label();
-			Checkbox chbRecord = new Checkbox();
-			final JButton btnStartProcess = new JButton(START_STOP_TXT);
-
-			cmbCOMPortNumber.setModel(new javax.swing.DefaultComboBoxModel<String>(USART.NUMBERS_OF_COM_PORTS));
-			cmbCOMPortNumber.setSelectedIndex(9);
-			cmbCOMPortNumber.addItemListener(new ItemListener() {
-				@Override
-				public void itemStateChanged(ItemEvent e) {
-					if(controller.isOpenUSARTPort()){
-						controller.closeUSARTPort(); // may be set delay
-						controller.turnOnUSART(cmbCOMPortNumber.getSelectedItem().toString());
-					}else{
-						controller.turnOnUSART(cmbCOMPortNumber.getSelectedItem().toString());
-					}
-				}
-			});
-
-			cmbNumberOfChannels.setModel(new javax.swing.DefaultComboBoxModel<String>(CHANNELS_NAMES));
-			cmbNumberOfChannels.setSelectedIndex(cmbNumberOfChannels.getItemCount() - 1);
-			cmbNumberOfChannels.addItemListener(new ItemListener() {
-				@Override
-				public void itemStateChanged(ItemEvent e) {
-					String[] availableSPS = new String[0];
-					try {
-						availableSPS = controller.setChannelsNum(cmbNumberOfChannels.getSelectedIndex());
-					} catch (LargeChannelsSetupException e1) {
-						e1.printStackTrace();
-					}
-					cmbSpsSelector.setModel(new javax.swing.DefaultComboBoxModel<String>(availableSPS));
-				}
-			});
-			cmbNumberOfChannels.setSelectedItem(0);
-
-			cmbSpsSelector.addItemListener(new ItemListener() {
-				@Override
-				public void itemStateChanged(ItemEvent e) {
-					controller.setMCUSamplesPerSecond((String) cmbSpsSelector.getSelectedItem());
-				}
-			});
-			cmbSpsSelector.setSelectedItem(0);
-
-			lbPortState.setFont(new Font("tahoma", Font.BOLD, 18));
-			lbPortState.setText(PORT_CLOSE_TXT);
-
-			chbRecord.setLabel(RECORD_TXT);
-			chbRecord.setFont(new Font("tahoma", Font.BOLD, 18));
-			chbRecord.setState(false); // record not set by default
-			chbRecord.addItemListener(new ItemListener() {
-				@Override
-				public void itemStateChanged(ItemEvent e) {
-					controller.doRecord();
-				}
-			});
-
-			btnStartProcess.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					controller.doStartStopMsr();
-				}
-			});
-
 			// Panel which placed direction elements
 			JPanel dirPanel = new JPanel();
 			dirPanel.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
@@ -164,7 +106,6 @@ public class View extends ApplicationFrame implements ViewCallback{
 			add(chartpanel);
 			add(dirPanel, BorderLayout.SOUTH);
 		}
-
 		/**
 		 * Create chart from 4 channels times series collection
 		 * @param xyCh1 data set to channel 1
@@ -204,6 +145,75 @@ public class View extends ApplicationFrame implements ViewCallback{
 			return jfreechart;
 		}
 
+	}
+
+	/**
+	 * Initialization and settings view components placed in direction panel.
+	 */
+	private void initDirPanelComponents() {
+		cmbCOMPortNumber.setModel(new DefaultComboBoxModel<String>(USART.NUMBERS_OF_COM_PORTS));
+		cmbCOMPortNumber.setSelectedIndex(9);
+		cmbCOMPortNumber.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if(controller.isOpenUSARTPort()){
+					controller.closeUSARTPort(); // may be set delay
+					controller.turnOnUSART(cmbCOMPortNumber.getSelectedItem().toString());
+				}else{
+					controller.turnOnUSART(cmbCOMPortNumber.getSelectedItem().toString());
+				}
+			}
+		});
+
+		cmbNumberOfChannels.setModel(new DefaultComboBoxModel<String>(CHANNELS_NAMES));
+		cmbNumberOfChannels.setSelectedIndex(cmbNumberOfChannels.getItemCount() - 1);
+		cmbNumberOfChannels.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				String[] availableSPS = new String[0];
+				try {
+					availableSPS = controller.setChannelsNum(cmbNumberOfChannels.getSelectedIndex());
+				} catch (LargeChannelsSetupException e1) {
+					e1.printStackTrace();
+				}
+				cmbSpsSelector.setModel(new DefaultComboBoxModel<String>(availableSPS));
+			}
+		});
+		cmbNumberOfChannels.setSelectedItem(0);
+
+		cmbSpsSelector.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				controller.setMCUSamplesPerSecond((String) cmbSpsSelector.getSelectedItem());
+			}
+		});
+		String[] availableSPS = new String[0];
+		try {
+			availableSPS = controller.setChannelsNum(cmbNumberOfChannels.getSelectedIndex());
+		} catch (LargeChannelsSetupException e) {
+			e.printStackTrace();
+		}
+		cmbSpsSelector.setModel(new DefaultComboBoxModel<String>(availableSPS));
+
+		lbPortState.setFont(new Font("tahoma", Font.BOLD, 18));
+		lbPortState.setText(PORT_CLOSE_TXT);
+
+		chbRecord.setLabel(RECORD_TXT);
+		chbRecord.setFont(new Font("tahoma", Font.BOLD, 18));
+		chbRecord.setState(false); // record not set by default
+		chbRecord.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				controller.doRecord();
+			}
+		});
+
+		btnStartProcess.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				controller.doStartStopMsr();
+			}
+		});
 	}
 
     static Class class$(String s){
